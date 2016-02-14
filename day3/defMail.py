32a05bdcc670164111051eb0230a07cad758a070
willianflasky <284607860@qq.com>
2016-02-15 07:18:29 +0800

    2016-2-15

--- day3/defMail.py
+++ day3/defMail.py
@@ -3,35 +3,24 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-"""
-def mail():
+def mail(user):
     ret=True
     try:
         msg = MIMEText('邮件内容', 'plain', 'utf-8')
         msg['From'] = formataddr(["吴老师",'wptawy@126.com'])
-        msg['To'] = formataddr(["willian",'@qq.com'])
+        #msg['To'] = formataddr(["willian",'284607861@qq.com'])
         msg['Subject'] = "主题"
 
         server = smtplib.SMTP("smtp.126.com", 25)
-        server.login("wptawy@126.com", "passwd")
-        server.sendmail('wptawy@126.com', ['@qq.com',], msg.as_string())
+        server.login("wptawy@126.com", "WW.3945.59")
+        server.sendmail('wptawy@126.com', [user,], msg.as_string())
         server.quit()
     except Exception:
         ret=False
     return ret
 
-ret = mail()
+ret = mail('284607860@qq.com')
 if ret:
     print('发送成功')
 else:
     print("发送失败")
-def show():
-    print('a')
-    if 1==2:
-        return [11,22]
-    print('b')
-
-ret=show()
-print(ret)
-
-"""
