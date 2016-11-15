71a7cc4be731c5943a82317047e3c2b96ac2f35a
willianflasky <284607860@qq.com>
2016-11-15 20:02:47 +0800

    django

---
+++ day17/repeat/extend2.js
@@ -0,0 +1,39 @@
+/**
+ * Created by willian on 16/11/11.
+ */
+
+(function(jq){
+      jq.extend({
+            'dalong2': function (arg) {
+             console.log(arg);
+        }
+    });
+
+    function f1(){
+
+    };
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+//
+//b=function () {
+//    $.extend({
+//        'dalong2': function (arg) {
+//            console.log(arg);
+//        }
+//    })
+//
+//    function f1(){
+//
+//    }
+//}
+//
+//b();
